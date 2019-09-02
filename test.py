e725db4d699ab6f14219836b28602db53602182c
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-09-02 10:04:28 +0300

    final

--- test.py
+++ test.py
@@ -2,39 +2,36 @@ def Keymaker(k):
   kst = []
   step = 0
   result = ''
+  num = []
+  o = []
   for j in range(k): 
     if step == 0: # первый этап
       for i in range(k):
-        kst.append(1)
+        kst.append('1')
       step += 1  
     else: # второй и последующие этапы
       for i in range(k):
         if i%(j+1) == j:
-          if kst[i] == 0:
-            kst.insert(i,1)
+          if kst[i] == '0':
+            kst.insert(i,'1')
             kst.pop(i+1)
           else:  
-            kst.insert(i,0)
+            kst.insert(i,'0')
             kst.pop(i+1)
+        if kst[i] == '1' and j == k-1:
+          num.append(i)
+        elif kst[i] == '0' and j == k-1:
+          o.append(i)
+    print(kst)
   for i in range(len(kst)):
     result += str(kst[i])
+  print('test - ',len(kst),num,o)
   return result
 
-print(Keymaker(1))
-print(Keymaker(2)) 
-print(Keymaker(3))
-print(Keymaker(4))
-print(Keymaker(5))
-print(Keymaker(6)) 
-print(Keymaker(7))
-print(Keymaker(8))
-print(Keymaker(9))
-print(Keymaker(10)) 
-print(Keymaker(11))
-print(Keymaker(12))
-print(Keymaker(13))
-print(Keymaker(14)) 
-print(Keymaker(15))
-print(Keymaker(16))
+#
+print(Keymaker(20))
+#for i in range(n):
+#  print(i,Keymaker(i))
 
-# 1 - открфтая дверь, 0 - закрытая дверь, 
+
+# 1 - открытая дверь, 0 - закрытая дверь, 
